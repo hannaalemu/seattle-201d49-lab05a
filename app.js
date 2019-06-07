2483192216118f5f625fed340efeb6f41c4048a0
hannaalemu <hanna9alemu@gmail.com>
2019-06-07 15:25:19 -0700

    no changes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function sum(a, b) { //eslint-disable-line
 
 console.log(sum(4, 7));
 
-// Here is the test for sum(); uncomment it to run it one time
+// Here is the test for sum(); uncomment it to run it one time no chnages
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
